17a86e7f5604cd5cc7bd3e4f388e063fcfd5e633
simransdsu <simran5590@gmail.com>
2022-07-04 11:43:53 -0700

    Initial Commit

---
+++ GRDBExample/GRDBExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GRDBExampleApp.swift
+//  GRDBExample
+//
+//  Created by Simran Preet Singh Narang on 2022-07-04.
+//
+
+import SwiftUI
+
+@main
+struct GRDBExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
